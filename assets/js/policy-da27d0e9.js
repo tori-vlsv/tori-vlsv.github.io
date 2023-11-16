57d41956f0c80b5fdf8f1768cf3378a1c7619444
tori-vlsv <victoriaavlasova@gmail.com>
2023-11-16 12:39:10 +0300

    update

--- assets/js/policy-da27d0e9.js
+++
@@ -1,1 +0,0 @@
-import"./style-72defbe0.js";import"./common-9ea17362.js";window.addEventListener("DOMContentLoaded",()=>{});
---
+++ assets/js/policy-e7ee53be.js
@@ -0,0 +1,1 @@
+import"./style-830dae82.js";import"./common-25771490.js";window.addEventListener("DOMContentLoaded",()=>{});
