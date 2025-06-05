4eaf6d686604fa74bbf6f0955f2a559eaf8f8736
tori-vlsv <victoriaavlasova@gmail.com>
2025-06-05 19:19:02 +0300

    update files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// модальное окно
 if (window.matchMedia("(max-width: 1023px)").matches) {
     const popup = document.querySelector('.hero-popup__wrapper');
     let startY = 0;
@@ -15,3 +16,37 @@ if (window.matchMedia("(max-width: 1023px)").matches) {
       }
     }, { passive: true });
 }
+
+// табсы
+
+const buttons = document.querySelectorAll('.tabs__button');
+const blocks = document.querySelectorAll('.tabs__block');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        blocks.forEach(block => block.classList.remove('active'));
+        
+        this.classList.add('active');
+        
+        const targetId = this.getAttribute('data-target');
+        const targetBlock = document.getElementById(targetId);
+        targetBlock.classList.add('active');
+    });
+});
+
+// слайдер
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
